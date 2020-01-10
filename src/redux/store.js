bfca2fbdcb9abee6e55fabe4fa2fc310d2469d66
xilian <573110413@qq.com>
2020-01-10 16:56:13 +0800

    Revert "feat: axios middleware (optimize API require)"
    This reverts commit c49bbe366c51b8978339c55d9131674636cc0b4d.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-// import thunkMiddleware from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 import combineReducers from '@redux/reducers.js';
 
-import promiseMiddleware from './middleware/promiseMiddleware';
-
-let store = createStore(combineReducers, applyMiddleware(promiseMiddleware));
+let store = createStore(combineReducers, applyMiddleware(thunkMiddleware));
 
 export default store;
